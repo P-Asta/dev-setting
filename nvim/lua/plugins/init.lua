9dd58a0c52f4315a9567252cc87b71c7d0e43e7a
5-23 <yhanbyeol6@gmail.com>
2024-10-24 22:33:33 +0900

    chore: change some config

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -18,14 +18,14 @@ return {
   {
     'mrcjkb/rustaceanvim',
     version = '^5', -- Recommended
-    lazy = false, -- This plugin is already lazy
+    lazy = false,   -- This plugin is already lazy
     ft = "rust",
-    config = function ()
+    config = function()
       local mason_registry = require('mason-registry')
       local codelldb = mason_registry.get_package("codelldb")
       local extension_path = codelldb:get_install_path() .. "/extension/"
       local codelldb_path = extension_path .. "adapter/codelldb"
-      local liblldb_path = extension_path.. "lldb/lib/liblldb.dylib"
+      local liblldb_path = extension_path .. "lldb/lib/liblldb.dylib"
       local cfg = require('rustaceanvim.config')
 
       vim.g.rustaceanvim = {
@@ -39,7 +39,7 @@ return {
   {
     'mfussenegger/nvim-dap',
     config = function()
-			local dap, dapui = require("dap"), require("dapui")
+      local dap, dapui = require("dap"), require("dapui")
       dap.listeners.before.attach.dapui_config = function()
         dapui.open()
       end
@@ -52,20 +52,20 @@ return {
       dap.listeners.before.event_exited.dapui_config = function()
         dapui.close()
       end
-		end,
+    end,
   },
 
   {
-    'rcarriga/nvim-dap-ui', 
-    dependencies = {"mfussenegger/nvim-dap", "nvim-neotest/nvim-nio"},
+    'rcarriga/nvim-dap-ui',
+    dependencies = { "mfussenegger/nvim-dap", "nvim-neotest/nvim-nio" },
     config = function()
-			require("dapui").setup()
-		end,
+      require("dapui").setup()
+    end,
   },
 
   {
     'saecki/crates.nvim',
-    ft = {"toml"},
+    ft = { "toml" },
     config = function()
       require("crates").setup {
         completion = {
@@ -75,7 +75,7 @@ return {
         },
       }
       require('cmp').setup.buffer({
-        sources = { { name = "crates" }}
+        sources = { { name = "crates" } }
       })
     end
   },
@@ -83,8 +83,8 @@ return {
     'akinsho/flutter-tools.nvim',
     lazy = false,
     dependencies = {
-        'nvim-lua/plenary.nvim',
-        'stevearc/dressing.nvim', -- optional for vim.ui.select
+      'nvim-lua/plenary.nvim',
+      'stevearc/dressing.nvim', -- optional for vim.ui.select
     },
     config = true,
   },
